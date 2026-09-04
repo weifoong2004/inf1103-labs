6023aa242b8fd25fa4b618843d6c8c24dd94d143
Wei Foong <weifoong2004@gmail.com>
2026-09-04 15:09:02 +0800

    Fifth Post

--- hello.py
+++ hello.py
@@ -34,3 +34,17 @@ print("====================")
 print("Username:", username)
 print("Age:", age)
 print("Category:", category)
+
+#Activity 5
+username =input("Enter Username: ")
+age =int(input("Enter Age: "))
+category =input("Enter Content Category: ")
+
+print("\nInstagram Profile")
+print("====================")
+print("Username:", username)
+print("Age:", age)
+print("Category:", category)
+
+if age > 40 and category == "fun":
+    print ("You are old what is fun for you??")
