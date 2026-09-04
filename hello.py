f60b74d009439c286bbd3ce6a76ed8560315322f
Wei Foong <weifoong2004@gmail.com>
2026-09-04 14:45:32 +0800

    Third Post

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
+# Activity 1 & 2
 print("============================")
 print("Welcome here")
 print("My first post")
@@ -10,3 +11,15 @@ followers = 100
 print("Username:", username)
 print("Bio:", bio)
 print("Followers:", followers)
+
+#Activity 3
+followers = 100
+
+followers += 50
+print ("Day 1:", followers)
+
+followers += 20
+print("Day 2:", followers)
+
+followers += 10
+print ("Day 3:", followers)
