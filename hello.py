217792dc221f8f44a77020303c9eb678c7f8d0c3
Wei Foong <weifoong2004@gmail.com>
2026-09-04 15:03:47 +0800

    Fourth Post

--- hello.py
+++ hello.py
@@ -23,3 +23,14 @@ print("Day 2:", followers)
 
 followers += 10
 print ("Day 3:", followers)
+
+#Activity 4
+username = input("Enter Username: ")
+age = input("Enter Age: ")
+category = input("Enter Content Category: ")
+
+print("\nInstagram Profile")
+print("====================")
+print("Username:", username)
+print("Age:", age)
+print("Category:", category)
